84c7cb8176381c048a8c29c78fe9724b25930d5a
December G <dgarnetsmith@gmail.com>
2023-07-12 18:44:39 -0500

    Move job views if statement to top

--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   const tabUrl = tab.url ?? tab.pendingUrl;
   if (
-    (changeInfo.status === "complete" &&
-      tabUrl &&
-      tabUrl.includes("linkedin.com/jobs/search")) ||
-    tabUrl.includes("linkedin.com/jobs/collections")
-  ) {
-    chrome.scripting.insertCSS({
-      target: { tabId: tabId },
-      files: ["css/search.css"],
-    });
-  } else if (
     changeInfo.status === "complete" &&
     tabUrl &&
     tabUrl.includes("linkedin.com/jobs/view")
@@ -18,6 +8,17 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     chrome.scripting.insertCSS({
       target: { tabId: tabId },
       files: ["css/view.css"],
+    });
+  } else if (
+    (changeInfo.status === "complete" &&
+      tabUrl &&
+      tabUrl.includes("linkedin.com/jobs/search")) ||
+    tabUrl.includes("linkedin.com/jobs/collections") ||
+    tabUrl.includes("linkedin.com/jobs")
+  ) {
+    chrome.scripting.insertCSS({
+      target: { tabId: tabId },
+      files: ["css/search.css"],
     });
   }
 });
